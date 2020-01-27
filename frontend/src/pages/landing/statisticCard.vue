7a4827b0e0b16e6af7c3f18bd572e97dff6fe1b1
Ice Lam <pinky.yy.lam@gmail.com>
2020-01-28 00:20:51 +0800

    feat(frontend): update statistic variable due to changes on scrap data

--- frontend/src/pages/landing/statisticCard.vue
+++ frontend/src/pages/landing/statisticCard.vue
@@ -3,16 +3,16 @@
     <div class="remarks" v-if="lastUpdate">截至 {{lastUpdate}}</div>
     <div class="row">
       <div class="column column--25">
-        <statistic :count="statisticCount.confirmedCount" label="宗確診病例" cardColor="red" />
+        <statistic :count="virusSummary.confirmedCount" label="宗確診病例" cardColor="red" />
       </div>
       <div class="column column--25">
-        <statistic :count="statisticCount.suspectedCount" label="宗懷疑個案" cardColor="yellow" />
+        <statistic :count="virusSummary.suspectedCount" label="宗懷疑個案" cardColor="yellow" />
       </div>
       <div class="column column--25">
-        <statistic :count="statisticCount.curedCount" label="宗治癒個案" cardColor="green" />
+        <statistic :count="virusSummary.curedCount" label="宗治癒個案" cardColor="green" />
       </div>
       <div class="column column--25">
-        <statistic :count="statisticCount.deadCount" label="宗死亡病例" cardColor="grey" />
+        <statistic :count="virusSummary.deadCount" label="宗死亡病例" cardColor="grey" />
       </div>
     </div>
   </div>
@@ -20,7 +20,7 @@
 
 <script>
 import { statistic } from '@components';
-import { extractDxyStatistic, formatDate } from '@utils';
+import { formatDate } from '@utils';
 
 export default {
   components: {
@@ -30,19 +30,6 @@ export default {
     virusSummary: {
       type: Object,
       required: true
-    }
-  },
-  data() {
-    return {
-      statisticCount: undefined
-    };
-  },
-  beforeMount() {
-    this.statisticCount = extractDxyStatistic(this.virusSummary.countRemark);
-  },
-  watch: {
-    virusSummary() {
-      this.statisticCount = extractDxyStatistic(this.virusSummary.countRemark);
     }
   },
   computed: {

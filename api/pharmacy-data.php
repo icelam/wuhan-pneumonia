d22be490be44b79d6bbadc6c94ae4967222b4540
Ice Lam <pinky.yy.lam@gmail.com>
2020-01-29 18:21:30 +0800

    feat(backend): scrap blacklist pharmacy from google spreadsheet

---
+++ api/pharmacy-data.php
@@ -0,0 +1,16 @@
+<?php
+// ini_set("display_errors", 1);
+// error_reporting(E_ALL);
+
+include 'scrapper/pharmacyScrapper.php';
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+
+$pharmacyScrapper = new pharmacyScrapper();
+$results = $pharmacyScrapper->scrapContent();
+echo $results;
+?>

2044bf297476902542e43834af197717d18a8f04
Ice Lam <pinky.yy.lam@gmail.com>
2020-02-03 13:25:18 +0800

    feat(backend): change data source of related news to RSS App

--- api/scrapper/googleNewsScrapper.php
+++ api/scrapper/googleNewsScrapper.php
@@ -3,8 +3,8 @@ require_once('baseScrapper.php');
 
 class googleNewsScrapper extends baseScrapper {
   public function __construct() {
-    // $this->scrap_url = "https://rss.app/feeds/6mbkDJ9rbsKORnFA.xml";
-    $this->scrap_url = "https://news.google.com/rss/search?q=%E8%82%BA%E7%82%8E&hl=zh-HK&gl=HK&ceid=HK%3Azh-Hant";
+    $this->scrap_url = "https://rss.app/feeds/6mbkDJ9rbsKORnFA.xml";
+    // $this->scrap_url = "https://news.google.com/rss/search?q=%E8%82%BA%E7%82%8E&hl=zh-HK&gl=HK&ceid=HK%3Azh-Hant";
     $this->store_folder = "scraps";
     $this->store_filename = "news.json";
   }
@@ -12,32 +12,35 @@ class googleNewsScrapper extends baseScrapper {
   public function postProcessing($data) {
     try {   
       // Convert scrapped rss to json
-      $extracted_rows = $this->convertRssToJson($data);
-      $api_output = json_encode($extracted_rows);
+      // TODO: non-hardcoded handling
+      $removedNS = str_replace("media:content", "mediaContent", $data);
+      $removedNS = str_replace("dc:creator", "creator", $removedNS);
+
+      $api_output = $this->convertRssToJson($removedNS);
 
       return $api_output;
     } catch (Exception $e) {
+      var_dump($e->getMessage());
       return NULL;
     }
   }
 
-  public function convertRssToJson($rss) {
-    $dom = new DOMDocument;
-    $dom->loadHTML($rss);
-    $result = [];
-    $xpath = new DOMXPath($dom);
-
-    foreach ($xpath->query('//channel/item') as $item) {
-      $data = [];
-      
-      if($item->childNodes->length) {
-        foreach($item->childNodes as $i) {
-          $data[$i->nodeName] = $i->nodeValue;
-        }
+  private function convertRssToJson($rss) {
+    $xml = simplexml_load_string($rss,"SimpleXMLElement", LIBXML_NOCDATA);
+    
+    // Convert publish time to timestamp
+    foreach ($xml->channel->item as $item) {
+      if (isset($item->pubDate)) {
+        $item->timestamp = strtotime($item->pubDate);
       }
 
-      array_push($result, $data);
+      // Strip tags in description
+      if (isset($item->description)) {
+        $item->description = strip_tags($item->description);
+      }
     }
+
+    $result = json_encode($xml);
 
     return $result;
   }

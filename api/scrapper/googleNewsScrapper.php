1225daaa8882bf230256637f9a18c753a9444ece
Ice Lam <pinky.yy.lam@gmail.com>
2020-02-06 11:41:52 +0800

    feat(backend): switch new source to google news feed

--- api/scrapper/googleNewsScrapper.php
+++ api/scrapper/googleNewsScrapper.php
@@ -3,20 +3,15 @@ require_once('baseScrapper.php');
 
 class googleNewsScrapper extends baseScrapper {
   public function __construct() {
-    $this->scrap_url = "https://rss.app/feeds/6mbkDJ9rbsKORnFA.xml";
-    // $this->scrap_url = "https://news.google.com/rss/search?q=%E8%82%BA%E7%82%8E&hl=zh-HK&gl=HK&ceid=HK%3Azh-Hant";
+    // $this->scrap_url = "https://rss.app/feeds/6mbkDJ9rbsKORnFA.xml";
+    $this->scrap_url = "https://news.google.com/rss/topics/CAAqLAgKIiZDQkFTRmdvTkwyY3ZNVEZtY2pFMWRERTFhQklGZW1ndFNFc29BQVAB?hl=zh-HK&gl=HK&ceid=HK:zh-Hant";
     $this->store_folder = "scraps";
     $this->store_filename = "news.json";
   }
   
   public function postProcessing($data) {
-    try {   
-      // Convert scrapped rss to json
-      // TODO: non-hardcoded handling
-      $removedNS = str_replace("media:content", "mediaContent", $data);
-      $removedNS = str_replace("dc:creator", "creator", $removedNS);
-
-      $api_output = $this->convertRssToJson($removedNS);
+    try { 
+      $api_output = $this->convertRssToJson($data);
 
       return $api_output;
     } catch (Exception $e) {
@@ -35,11 +30,6 @@ class googleNewsScrapper extends baseScrapper {
     foreach ($xml->channel->item as $item) {
       if (isset($item->pubDate)) {
         $item->timestamp = strtotime($item->pubDate);
-      }
-
-      // Strip tags in description
-      if (isset($item->description)) {
-        $item->description = strip_tags($item->description);
       }
     }
 

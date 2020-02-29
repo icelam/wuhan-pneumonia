bfdaaeeb562d6006315e76c6cd5cca168616309b
Ice Lam <pinky.yy.lam@gmail.com>
2020-02-29 15:37:54 +0800

    feat(backend): switch to new home confinees data source

--- api/hk-home-confinees-building-data.php
+++ api/hk-home-confinees-building-data.php
@@ -1,12 +1,12 @@
 <?php
-include 'scrapper/govCsvScrapper.php';
+include 'scrapper/baseScrapper.php';
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header('Content-Type: application/json');
 
-$govCsvScrapper = new govCsvScrapper("https://www.chp.gov.hk/files/misc/building_list_home_confinees_chi.csv", "home-confinees-building.json");
+$govCsvScrapper = new baseScrapper("https://api.data.gov.hk/v1/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Fhome_confinees_tier2_building_list.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%2C%22filters%22%3A%5B%5B1%2C%22gt%22%2C%5B%220%22%5D%5D%5D%7D", "home-confinees-building.json");
 $results = $govCsvScrapper->scrapContent();
 echo $results;
 ?>
